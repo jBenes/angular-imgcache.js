741c1caa6d9ead6b07d55fd4a22d47323b807815
Benoit Sautel <benoit@goalmap.com>
2016-07-11 11:10:03 +0200

    Use non-cached image when it cannot be cached

--- angular-imgcache.js
+++ angular-imgcache.js
@@ -44,16 +44,19 @@ angular.module('ImgCache', [])
             icSrc: '@'
         },
         link: function(scope, el, attrs) {
+            var setImg = function(type, el, img_src) {
+                if (type === 'bg') {
+                    el.css({'background-image': 'url(' + img_src + ')'});
+                } else {
+                    el.attr('src', img_src);
+                }
+            };
 
-            var setImg = function(type, el, src) {
+            var setImgFromCache = function(type, el, src) {
 
                 ImgCache.getCachedFileURL(src, function(src, dest) {
                     var img_src = dest.fullPath.replace('/'. ImgCache.options.localCacheFolder, ImgCache.getCacheFolderURI());
-                    if(type === 'bg') {
-                        el.css({'background-image': 'url(' + img_src + ')' });
-                    } else {
-                        el.attr('src', img_src);
-                    }
+                    setImg(type, el, img_src);
                 });
             };
 
@@ -64,9 +67,11 @@ angular.module('ImgCache', [])
                     ImgCache.isCached(src, function(path, success) {
 
                         if (success) {
-                            setImg(type, el, src);
+                            setImgFromCache(type, el, src);
                         } else {
                             ImgCache.cacheFile(src, function() {
+                                setImgFromCache(type, el, src);
+                            }, function() {
                                 setImg(type, el, src);
                             });
                         }
